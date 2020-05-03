db805149017a656e8751c3c1de51ac99c064521f
Austin Cory Bart <acbart@vt.edu>
2020-05-03 17:16:16 -0400

    Updates

---
+++ dev/precache-manifest.78acd943a83cc0413744b238c8b3b663.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cb70427000177c37b2d089fafaa04d",
+    "url": "/phaser-feedback-collision/devassets/ada.png"
+  },
+  {
+    "revision": "431b99d2aa2576def6aa2b940c6fff1f",
+    "url": "/phaser-feedback-collision/devassets/blue.png"
+  },
+  {
+    "revision": "df7a38ddf5a75b5c1855de9eee452c62",
+    "url": "/phaser-feedback-collision/devassets/boom.wav"
+  },
+  {
+    "revision": "9f81d71575bba53a746144dae01e9e46",
+    "url": "/phaser-feedback-collision/devassets/bounce.wav"
+  },
+  {
+    "revision": "fae3f6531c250f6a1f1e4984a9471860",
+    "url": "/phaser-feedback-collision/devassets/campus.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/phaser-feedback-collision/devassets/citations.md"
+  },
+  {
+    "revision": "4ec18a8325ffb6d970c2760ba45923e2",
+    "url": "/phaser-feedback-collision/devassets/east_campus.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/phaser-feedback-collision/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/phaser-feedback-collision/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/phaser-feedback-collision/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/phaser-feedback-collision/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/phaser-feedback-collision/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "0ed661ec6645c74bba04c82ea1cfa2f5",
+    "url": "/phaser-feedback-collision/devassets/potato.png"
+  },
+  {
+    "revision": "988b2908c0a39b4586a187e2958915d1",
+    "url": "/phaser-feedback-collision/devassets/red.png"
+  },
+  {
+    "revision": "75dad167a793618cfd4bbf267943d3fb",
+    "url": "/phaser-feedback-collision/devassets/smith.png"
+  },
+  {
+    "revision": "af2aa279f355dd8f1400672f323058ab",
+    "url": "/phaser-feedback-collision/devassets/soon.png"
+  },
+  {
+    "revision": "3d7449c591f6334f15148690e1e90623",
+    "url": "/phaser-feedback-collision/devassets/trabant.png"
+  },
+  {
+    "revision": "34761f7f09b4a64431b03fdab73ca9d7",
+    "url": "/phaser-feedback-collision/devassets/whiteboard_corgi.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/phaser-feedback-collision/devfavicon.ico"
+  },
+  {
+    "revision": "89424ed29fbcd5815e87",
+    "url": "/phaser-feedback-collision/devgen.main.3ded103644a79b2cb4b6.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/phaser-feedback-collision/devgen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "90e7bcd5d35f37979ae16025bef333a0",
+    "url": "/phaser-feedback-collision/devindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/phaser-feedback-collision/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/phaser-feedback-collision/devsw.js"
+  }
+]);
